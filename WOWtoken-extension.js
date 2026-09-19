80331914f2e7fce582877a0acbf1e2fbb336c8cb
zzsleep <211677050+danvPR@users.noreply.github.com>
2026-09-19 21:56:23 +0700

    Fixed

--- WOWtoken-extension.js
+++ WOWtoken-extension.js
@@ -89,7 +89,7 @@
     getInfo() {
       return {
         id: "danvWowEconomy",
-        name: "Ví WOW DANV",
+        name: "WOW Economy"
         color1: "#f59e0b", // Vàng cam năng lượng
         color2: "#d97706",
         color3: "#b45309",
